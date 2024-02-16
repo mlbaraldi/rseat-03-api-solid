3afb785ddb36ec4f71b3a74b8213419a949db35a
Marcelo Baraldi <mlbaraldi@hotmail.com>
2024-02-16 17:20:34 -0300

    chore: add prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
+prisma.user.create({
+  data: {
+    name: 'Marcelo',
+    email: 'mlbaraldi@hotmail.com',
+  },
+})
 export const app = fastify()
